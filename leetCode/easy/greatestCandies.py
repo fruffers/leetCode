ab2cbd67458d58ce934b0a5a2181f1ba4d7d4137
Louise Bilous <bywater30@gmail.com>
2020-12-26 16:25:33 +0000

    Add description

--- leetCode/easy/greatestCandies.py
+++ leetCode/easy/greatestCandies.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# Kids with the Greatest Number of Candies
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         counter1 = -1
